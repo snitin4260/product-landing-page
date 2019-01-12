b554c92a7eec943061e7299e3d5fc85e23728ef5
nitin <snitin8994@gmail.com>
2019-01-12 13:16:55 +0530

    Add favicon

---
+++ api/goolgeBooks.js
@@ -0,0 +1,7 @@
+// import axios from "axios";
+
+// axios.get('https://www.googleapis.com/books/v1/volumes',{
+//     params: {
+//         q: "dance"
+//     }
+// });
--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+import axios from "axios";
+
+
 let hamburger = document.querySelector("#hamburger");
 let navbar = document.querySelector("#nav-bar");
 let closeIcon = document.querySelector("#nav-bar .close")
+let body = document.querySelector("body");
 
 hamburger.addEventListener("click",function() {
 
@@ -17,3 +21,28 @@ closeIcon.addEventListener("click",function() {
     closeIcon.classList.remove("show");
     hamburger.classList.remove("hide");
 });
+
+async function books() {
+
+    let result = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+        params: {
+            q: "atomic+isbn=1847941834,",
+            
+            key: 'AIzaSyBLRyH5Njmh1T_MD5uKueLTv9bqJfUm5zM'
+        }
+    });
+    
+    console.log(result.data.items[0].volumeInfo.imageLinks.thumbnail);
+
+    let img =document.createElement("img");
+    img.src = result.data.items[0].volumeInfo.imageLinks.thumbnail;
+    img.setAttribute("alt","book");
+
+    body.appendChild(img);
+    
+
+
+
+}
+
+books();
