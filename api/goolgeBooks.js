c0f42cc2f306f5c16c411fff6a90420fed28f27e
nitin <snitin8994@gmail.com>
2019-01-13 00:00:27 +0530

    Add Book column

--- api/goolgeBooks.js
+++
@@ -1,7 +0,0 @@
-// import axios from "axios";
-
-// axios.get('https://www.googleapis.com/books/v1/volumes',{
-//     params: {
-//         q: "dance"
-//     }
-// });
--- js/index.js
+++ js/index.js
@@ -1,48 +1,85 @@
-import axios from "axios";
-
-
+import nyt from "../api/newYorkTimes";
 let hamburger = document.querySelector("#hamburger");
 let navbar = document.querySelector("#nav-bar");
-let closeIcon = document.querySelector("#nav-bar .close")
-let body = document.querySelector("body");
+let closeIcon = document.querySelector("#nav-bar .close");
+let booksContainer = document.querySelector("#latest-books");
+let loader = document.querySelector(".loader_container");
 
-hamburger.addEventListener("click",function() {
+function createElem(elem, className) {
+  let el = document.createElement(elem);
+  if (className) {
+    el.classList.add(className);
+  }
 
-    navbar.classList.add("open");
-    //display close icon in navbar whose initial display was none
-    closeIcon.classList.add("show");
-    hamburger.classList.add("hide")
-
-})
-
-
-closeIcon.addEventListener("click",function() {
-    navbar.classList.remove("open");
-    closeIcon.classList.remove("show");
-    hamburger.classList.remove("hide");
-});
-
-async function books() {
-
-    let result = await axios.get('https://www.googleapis.com/books/v1/volumes', {
-        params: {
-            q: "atomic+isbn=1847941834,",
-            
-            key: 'AIzaSyBLRyH5Njmh1T_MD5uKueLTv9bqJfUm5zM'
-        }
-    });
-    
-    console.log(result.data.items[0].volumeInfo.imageLinks.thumbnail);
-
-    let img =document.createElement("img");
-    img.src = result.data.items[0].volumeInfo.imageLinks.thumbnail;
-    img.setAttribute("alt","book");
-
-    body.appendChild(img);
-    
-
-
-
+  return el;
 }
 
-books();
+function createBookGrid(allBookLists) {
+  //allBooklist is an array with 15 items
+  // each item is a booklist say fiction
+  //which has 5 books each
+
+  let allLists = createElem("div", "all-lists");
+  for (let i = 0; i < allBookLists.length; i++) {
+    let bookList = allBookLists[i]; //booklist is an object
+
+    let list = createElem("div", "list");
+
+    let listName = createElem("h3", "list_name");
+    //get list name say fiction from the list
+    listName.textContent = bookList.list_name;
+
+    list.appendChild(listName);
+
+    let listGridContainer = createElem("div", "list_gridContainer");
+    list.appendChild(listGridContainer);
+
+    for (let j = 0; j < bookList.books.length; j++) {
+      let book = bookList.books[j];
+      let listItem = createElem("div", "list_item");
+
+      //book details
+      listItem.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <img src=${book.book_image} class="list_image" alt="${
+          book.title
+        } image" >
+      <p class="list_title">${book.title}</p>
+      <p class="list_author">${book.author}</p>
+      <p class="list_description">${book.description}</p>
+      <p class="list_publisher">${book.publisher}</p>
+      <p class="list_isbn"> ${book.primary_isbn10}</p>
+      `
+      );
+
+      listGridContainer.appendChild(listItem);
+    }
+
+    allLists.appendChild(list);
+  }
+
+  booksContainer.appendChild(allLists);
+  loader.style.display = "none";
+}
+
+hamburger.addEventListener("click", function() {
+  navbar.classList.add("open");
+  //display close icon in navbar whose initial display was none
+  closeIcon.classList.add("show");
+  hamburger.classList.add("hide");
+});
+
+closeIcon.addEventListener("click", function() {
+  navbar.classList.remove("open");
+  closeIcon.classList.remove("show");
+  hamburger.classList.remove("hide");
+});
+
+async function getResponseFromNYT() {
+  let response = await nyt;
+  let allBookLists = response.data.results.lists;
+  createBookGrid(allBookLists);
+}
+
+getResponseFromNYT();
