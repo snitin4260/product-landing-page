abb6ab6fa04d95ffa5f53f608dab1ce6d3ea1f77
nitin <snitin8994@gmail.com>
2019-01-16 13:48:37 +0530

    Add new key

--- api/newYorkTimes.js
+++ api/newYorkTimes.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let baseURL = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
-let apiKey = "787cd9d413f64485905e74b194a6548e";
+let apiKey = "COvXyTMbyTFArxUuNn9o42nXhWCcobkE";
 
 export default axios.get(baseURL, {
   params: {
